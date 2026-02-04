773f8231b96364ec5f9351d2eaa67e182f880a91
Best <bestsakditat@gmail.com>
2026-02-04 09:52:19 +0700

    setup and install project

--- app.mjs
+++ app.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4000;
@@ -8,6 +9,23 @@ app.use(express.json());
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
+
+app.get("/test-db" , async (req,res) => {
+  try {
+    const results = await connectionPool.query(`SELECT * FROM answers`)
+    return res.status(200).json ({
+      data : results.rows
+    })
+
+  }catch (error) {
+    res.status(500).json({
+      message : "database don't working"
+    })
+    console.log(error)
+  }
+})
+
+
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
--- utils/db.mjs
+++ utils/db.mjs
@@ -4,7 +4,7 @@ const { Pool } = pg.default;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://postgres:bzforest@localhost:5432/BackEnd-Skillcheckpoint",
 });
 
 export default connectionPool;
